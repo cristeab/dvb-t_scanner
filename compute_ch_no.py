71c3734b5c501b65dd2037d4d17eb8b7ddbdea5b
Bogdan Cristea <cristeab@gmail.com>
2012-09-21 10:25:17 +0200

    corr

--- compute_ch_no.py
+++ compute_ch_no.py
@@ -10,12 +10,9 @@ fec_up = (2, 3)
 fec_low = (3, 4)
 guard_up = (1, 1)
 guard_low = (32, 8)
-#idx2 = 1
 
-#freq = input('F[MHz] = ')
-for idx2 in range(0, 2):
+for idx2 in range(0, len(fec_up)):
     for ch_no in range(ch_min, ch_max+1):
-#    ch_no = (freq-306)/8
         freq_MHz = 8*ch_no+306
         print 'T', int(freq_MHz*1e6+freq_shift_kHz[idx]*1e3), '8MHz '+\
         str(fec_up[idx2])+'/'+str(fec_low[idx2])+' NONE QAM64 8k '+\
