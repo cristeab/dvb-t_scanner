70a9fd4f1c70540b358089b241c1aaa30d8e5195
Bogdan Cristea <cristeab@gmail.com>
2012-09-21 12:04:07 +0200

    add options

--- compute_ch_no.py
+++ compute_ch_no.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python
 
-#T 474166000 8MHz 2/3 NONE QAM64 8k 1/32 NONE
+#Brief: Scans DVB-T channels in order to find the available stations for a specific location
+#Author: Bogdan Cristea
 
-ch_min = 21
-ch_max = 69
+#Uses 'scan' utility which must be in the system path. The scan utility receives as input a text file,
+#so that on each line a DVB-T channel is defined.
+#Each DVB-T channel is defined by the following parameters:
+#T 474166000 8MHz 2/3 NONE QAM64 8k 1/32 NONE
+#The output represents the detected DVB-T stations and can be used as input to VLC, for example.
+
+from optparse import OptionParser
+from subprocess import call
+from sys import exit
+
+#set input options
+parser = OptionParser()
+parser.add_option("-m", "--channel-min", dest="ch_min",
+    help="DVB-T channel minimum index (default 21)",
+    default=21)
+parser.add_option("-M", "--channel-max", dest="ch_max",
+    help="DVB-T channel maximum index (default 69)",
+    default=69)
+parser.add_option("-l", "--location", dest="location", 
+    help="location (town, address, etc.) where the scan is realized (default empty string)",
+    default="")
+parser.add_option("-s", "--freq-shift-index", dest="freq_shift_idx", 
+    help="frequency shift index of the DVB channel (default 1)",
+    default=1)
+(options, args) = parser.parse_args()
+
+#scan parameters
+ch_min = int(options.ch_min) # default 21
+ch_max = int(options.ch_max) # default 69
+idx = int(options.freq_shift_idx) #doesn't need to be changed (default 1)
+#constants
 freq_shift_kHz = (-166, 166, 332, 498)
-idx = 1 #doesn't need to be changed (default 1)
 fec_up = (2, 3)
 fec_low = (3, 4)
 guard_up = (1, 1)
 guard_low = (32, 8)
+
+#check input
+if not(idx in range(0, len(freq_shift_kHz))):
+  print("Frequency shift index is out of range (min 0, max "+str(len(freq_shift_kHz)-1)+")")
+  exit(1)
+
+#show summary
+print("Scanning DVB-T channels from "+str(ch_min)+" to "+str(ch_max))
+print("Carrier frequency offset "+str(freq_shift_kHz[idx])+" kHz")
 
 for idx2 in range(0, len(fec_up)):
     for ch_no in range(ch_min, ch_max+1):
